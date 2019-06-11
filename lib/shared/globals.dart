0e4b104a63fd8dad3c92bdbf00f18a4c74da1090
januwA <ajanuw1641845087@sina.com>
2019-06-11 17:08:59 +0800

    修改启动图标

--- lib/shared/globals.dart
+++ lib/shared/globals.dart
@@ -1,7 +1,7 @@
 library video_app.globals;
 
 
-final String baseUrl = '192.168.0.107:3000';
+final String baseUrl = '192.168.0.103:3000';
 final String weekDataUrl = '/weekdata';
 final String detailUrl = '/detail';
 final String videoSrcUrl = '/src';
