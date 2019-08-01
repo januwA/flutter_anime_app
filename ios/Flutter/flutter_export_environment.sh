d1b24d228f9c9812c46ed8f8ed53decb62b254e7
januwA <ajanuw1641845087@sina.com>
2019-08-01 22:16:28 +0800

    debug

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\my-tools\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\ajanuw\flutter_anime_app"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\my-tools\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=0.4.1"
+export "FLUTTER_BUILD_NUMBER=0.4.1"
