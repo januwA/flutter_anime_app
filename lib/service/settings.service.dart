2d95f0f3f1321437b2c910f9e05dbb869f1a7c3a
januwA <ajanuw1995@gmail.com>
2021-01-06 22:52:19 +0800

    debug

--- lib/service/settings.service.dart
+++ lib/service/settings.service.dart
@@ -2,20 +2,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsService {
   static final String _key = 'proxyAddress';
-  SettingsService() {
-    proxyAddress = _dProxyAddress;
-    SharedPreferences.getInstance().then((value) {
-      _prefs ??= value;
-      proxyAddress = _prefs.getString(_key);
-    });
-  }
-
-  SharedPreferences _prefs;
 
   /// 'http://96.45.181.208:6677/api/nicotv?url='
   /// 小黄鸡服务器: 'http://47.93.223.14:6677/api/nicotv?url='
   /// 默认代理地址
   final _dProxyAddress = '47.93.223.14:6677';
+
+  SettingsService() {
+    proxyAddress = _dProxyAddress;
+    SharedPreferences.getInstance().then((value) {
+      _prefs ??= value;
+      String cache = _prefs.getString(_key);
+      if (cache.isNotEmpty) proxyAddress = cache;
+    });
+  }
+
+  SharedPreferences _prefs;
 
   String proxyAddress;
 
